a07dbfcecf4eb169718bd8ef9d82da88b903fa7a
Kaytster <ksharp2405@gmail.com>
2024-11-29 12:20:17 +0000

    css Setup

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,20 @@
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body>{children}</body>
+      <body>
+        
+        <div className='container'>
+        <header>
+          <h1>My Next Page</h1>
+        </header>
+        {children}
+        </div>
+
+        <footer>
+            <p>&copy; {new Date().getFullYear()} My Film Listing</p>
+        </footer>
+
+      </body>
     </html>
   );
 }
