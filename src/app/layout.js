d96d1d144ad9c0b05b7b9d1ecc17cdbb8b81f2a0
Kaytster <ksharp2405@gmail.com>
2024-12-10 20:12:53 +0000

    responsive

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,8 @@
 import Header from "@/app/components/header";
+import Head from "next/head";
 import "@/app/global.css";
-import "@/app/styles/mobile.css";
-import "@/app/styles/desktop.css";
+// import "@/app/styles/mobile.css";
+// import "@/app/styles/desktop.css" ;
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
+import Image from "next/image.js";
 import "./layout.js";
-import IndexPage from './components/indexpage';
-//import myImages from "./components/images.js"
+
 
 export const metadata = {
   title: "Home",
@@ -15,11 +15,44 @@ const Home = () => {
         <p>My Slideshow</p>
       </section>
 
+      <section id="links">
+      <div className="row">
+        <div className="col">
+          {/* <Image src="/Images/about-us.webp" alt="About Us" width={100} height={100} />  */}
+          <h3>About Us</h3>
+        </div>
+        <div className="spaceCol" />
+        <div className="col">
+        {/* <Image src="/Images/facilities.webp" alt="About Us" width={100} height={100} />  */}
+        <h3>Our Facilities</h3>
+        </div>
+        <div className="spaceCol" />
+        <div className="col">
+        {/* <Image src="/Images/courses.jpg" alt="About Us" width={100} height={100} /> */}
+        <h3>Our Courses</h3>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col">
+          {/* <Image src="/Images/resources.avif" alt="About Us" width={100} height={100} />  */}
+          <h3>Learning Resources</h3>
+        </div>
+        <div className="spaceCol" />
+        <div className="col">
+        {/* <Image src="/Images/facilities.webp" alt="About Us" width={100} height={100} />  */}
+        </div>
+        <div className="spaceCol" />
+        <div className="col">
+        {/* <Image src="/Images/courses.jpg" alt="About Us" width={100} height={100} /> */}
+        </div>
+      </div>
+    </section>
+
       <br />
       <div id="divider" />
       <br />
 
-      <IndexPage />
     </main>
   );
 };
