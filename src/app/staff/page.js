e5f82e9ce53056a579669d6907407d8fabc81727
Kaytster <ksharp2405@gmail.com>
2024-12-18 16:32:51 +0000

    staff

--- src/app/staff/page.js
+++ src/app/staff/page.js
@@ -9,26 +9,27 @@ export default function Staff() {
       <div>
         <h2>Information for Staff</h2>
         <h3>News</h3>
-        Cantor College recently hosted the Software Engineering student of the year awards. Short-listed candidates from around the country attended the one day event.
+        <p>Cantor College recently hosted the Software Engineering student of the year awards. </p>
+        <p>  Short-listed candidates from around the country attended the one day event.</p>
         <h3>Academic Registry</h3>
-        For academic regulations, assessment, awards, student records and course validation.
+        <p>For academic regulations, assessment, awards, student records and course validation.</p>
         <h3>Catering Services</h3>
-        Contact the Catering Services regarding on and off-site catering.
+        <p>Contact the Catering Services regarding on and off-site catering.</p>
         <h3>Financial Services</h3>
-        The financial team based on the 2nd Floor are responsible for all areas of student finance as 
-        well as College budgeting.
+        <p>The financial team based on the 2nd Floor are responsible for all areas of student finance as 
+        well as College budgeting.</p>
         <h3>Information Systems Services</h3>
-        The ISS team delivers the College's computing facilities including all hardware and software. 
-        They also run the staff helpdesk.
+        <p>The ISS team delivers the College's computing facilities including all hardware and software. </p>
+        <p> They also run the staff helpdesk.</p>
         <h3>Marketing Services</h3>
-        The Marketing Team will help promote events and new courses. They will help with press 
-        release preparation.
+        <p>The Marketing Team will help promote events and new courses. They will help with press 
+        release preparation.</p>
         <h3>Personnel Services</h3>
-        All staff pay and conditions enquiries should be directed to the Personnel Services team on 
-        the 3rd floor.
+        <p>All staff pay and conditions enquiries should be directed to the Personnel Services team on 
+        the 3rd floor.</p>
         <h3>Facilities</h3>
-        The facilities are responsible for the general care and maintenance of the College. All 
-        enquiries via the main helpdesk.
+        <p>The facilities are responsible for the general care and maintenance of the College. All 
+        enquiries via the main helpdesk.</p>
       </div>
     );
   }
