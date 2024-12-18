eb49f76cfe27a09ba9e0b77baf6ff97bbd0a5235
Kaytster <ksharp2405@gmail.com>
2024-12-18 14:07:16 +0000

    nav bar

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -15,8 +15,14 @@ const Navbar = () => {
       <button className='burger' onClick={toggleMenu}>&#9776;</button>
       <menu className={menuOpen ? "active" : ""}>
         <li><Link href="/" onClick={toggleMenu}>Home</Link></li>
-        <li><Link href="/students" onClick={toggleMenu}>Students</Link></li>
+        <li><Link href="/about" onClick={toggleMenu}>About</Link></li>
+        <li><Link href="/facilities" onClick={toggleMenu}>Facilities</Link></li>
+        <li><Link href="/courses" onClick={toggleMenu}>Courses</Link></li>
+        <li><Link href="/resources" onClick={toggleMenu}>Resources</Link></li>
         <li><Link href="/staff" onClick={toggleMenu}>Staff</Link></li>
+        <li><Link href="/students" onClick={toggleMenu}>Students</Link></li>
+        <li><Link href="/businesses" onClick={toggleMenu}>Businesses</Link></li>
+        <li><Link href="/contact" onClick={toggleMenu}>Contact</Link></li>
       </menu>
     </nav>
   );
